33eee83c73adfaeefd034b2567d793bd31df2ba8
frangiuliano <francosebastiangiuliano@gmail.com>
2023-12-10 22:39:50 -0300

    Add basic structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div class='flex items-center justify-center w-screen h-screen' >
+        <p>Hola Mundo!</p>
+      </div>
+  )
+}
+
+export default App
